527a9ea58a98663d90feaf23250856c1b023456c
Jordan Giacone <Jmgiacone@gmail.com>
2013-11-16 07:16:15 -0600

    Very minor changes to this class. Haven't even incorporated the new variables yet

--- hw10/Customer.cpp
+++ hw10/Customer.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
 #include "Customer.h"
+
+using namespace std;
+
+bool Customer::purchase(const Product item)
+{
+  if(m_numPurchases < MAX_PURCHASES)
+  {
+    m_purchases[m_numPurchases] = item;
+    m_numPurchases++;
+
+    return true;
+  }
+
+  return false;
+}
+  
+void Customer::print() const
+{
+  cout << m_name << " has $" << m_money << " and purchases: [";
+  
+  for(int i = 0; i < m_numPurchases; i++)
+  {
+    //cout << m_purchases[i] << (i == m_numPurchases - 1 ? "" : ", ");
+  } 
+  
+  cout << "]";
+}
--- hw10/Customer.h
+++ hw10/Customer.h
@@ -1,8 +1,31 @@
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
 
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "Product.h"
+
+using namespace std;
+const int MAX_PURCHASES = 20;
+const float MIN_MONEY = 4.0, MAX_MONEY = 250;
 class Customer
 {
-    
+  private:
+    string m_name, m_inclination;
+    Product m_purchases[MAX_PURCHASES];
+    float m_money;
+    short m_numPurchases, m_happiness;    
+
+  public:
+    Customer(const string name = "", const float money = 
+             ((rand() % static_cast<int>((100 * (MAX_MONEY - MIN_MONEY + 1) + MIN_MONEY))) / 100.0)) : 
+             m_name(name), m_money(money), m_numPurchases(0) {};
+    bool purchase(const Product item);
+    string getName() const {return m_name;};
+    float getMoney() const {return m_money;};
+    short getHappiness() const {return m_happiness;};
+    void setMoney(const float m) {m_money = m;};
+    void print() const;
 };
 #endif
