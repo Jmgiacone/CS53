b78210649c4f292dcf1f3e301d630df0bb7fc8ea
Jordan Giacone <Jmgiacone@gmail.com>
2013-11-16 07:14:44 -0600

    Updated the Business class and related functions. The bodies haven't changed much, but I added new variables into the class

--- hw10/Business.cpp
+++ hw10/Business.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
 #include "Business.h"
+
+using namespace std;
+
+void Business::print() const
+{
+  cout << "Business Name: " << m_name <<  endl;
+  cout << "Cash in register: $" << m_money << endl;
+  cout << "Customers: ";
+
+  for(int i = 0; i < m_numCustomers; i++)
+  {
+    cout << "\n  -";
+    m_customers[i].print();
+    cout << endl;
+  }
+
+  cout << endl;
+  cout << "Wares: [";
+
+  for(int i = 0; i < m_numWares; i++)
+  {
+    //cout << m_wares[i] << (i == m_numWares - 1 ? "" : ", ");
+  }
+  
+  cout << "]";
+}
+
+bool Business::addCustomer(Customer& c)
+{
+  if(m_numCustomers < MAX_CUSTOMERS)
+  {
+    m_customers[m_numCustomers] = c;
+    m_numCustomers++;
+    return true;
+  }
+  
+  return false;
+}
+
+void Business::sell_stuff()
+{
+
+}
+
+void customers_leave(Customer customers[], const int numCustomers)
+{
+  
+}
--- hw10/Business.h
+++ hw10/Business.h
@@ -1,6 +1,53 @@
 #ifndef BUSINESS_H
 #define BUSINESS_H
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include "Product.h"
+#include "Customer.h"
+
+using namespace std;
+const int MAX_WARES = 10, MAX_CUSTOMERS = 10, ITEM_PRICE = 40;
+
 class Business
 {
+  private:
+    string m_name;
+    float m_money;
+    Customer m_customers[MAX_CUSTOMERS];
+    Product m_toSell[MAX_WARES];
+    short m_numWares, m_numCustomers;
+  
+  public:
+    Business(const string name = "", const float money = 0, 
+             string fileName = "") : m_name(name), m_money(money), m_numWares(0)
+             , m_numCustomers(0)
+    {
+      /*if(fileName != "")
+      {
+        ifstream input;
+        char temp[100];
+
+        input.open(fileName.c_str());
+      
+        for(int i = 0; input; i++)
+        {
+          input.getline(temp, 100);
+          m_wares[i] = temp;
+          m_numWares++;
+        }
+        
+        m_numWares--;
+        input.close();
+      }*/
+    };
+
+    void print() const;
+    bool addCustomer(Customer& c);
+    void sell_stuff();
+    void customers_leave(Customer customers[], const int numCustomers); 
+    
 };
 #endif
