5bfc68f8fa52b6397380c743cda666cedd8010d1
Jordan Giacone <Jmgiacone@gmail.com>
2013-12-03 17:08:21 -0600

    fully revamped main to work better. For some reason, it segfaults

--- hw10/main.cpp
+++ hw10/main.cpp
@@ -11,43 +11,93 @@ using namespace std;
 
 const int NUM_OF_CUSTOMERS = 20;
 
+void removeIndex(Customer list[], const int index, const int size);
 int main()
 {
   srand(time(NULL));
-  bool theifOrPitcher;
-  bool here;
-  int unluckyPerson;
-  int desperateCustomers = 0;
-  int cycles = 0;
-  string custName;
-  string store;
-  Customer temporary; //Used for sorting
+  
+  int numCustomers = 0, numCycles = 0;
+  string custName, store;
   Customer list[NUM_OF_CUSTOMERS];
-  Customer listOfDesperation[NUM_OF_CUSTOMERS];
-  ifstream custlist;
-  custlist.open("CustomerList.txt");
-  Business MoeBar("Moe's", 0 ,"MoeBar.txt");
-  Business CBGStore("CBG's", 0, "CBGStore.txt");
-  for (int i=0; i<NUM_OF_CUSTOMERS; i++)
+  ifstream custlist("CustomerList.txt");
+
+  Business MoesBar("Moe's Bar", 0 ,"MoeBar.txt"), CBGStore("Comic Book Guy's Store", 0, "CBGStore.txt");
+
+  for (int i = 0; i < NUM_OF_CUSTOMERS; i++)
   {
     getline(custlist,custName,',');
     list[i].setName(custName);
     getline(custlist,store,'\n');
+    
     if (store == " -1")
     {
-      MoeBar.addCustomer(list[i]);
-      store = MOES_BAR;
+      list[i].setInclination(MOES_BAR);
     }
     else if (store == " 1")
     {
-      CBGStore.addCustomer(list[i]);
-      store = COMIC_BOOK_STORE;
+      list[i].setInclination(COMIC_BOOK_STORE);
     }
     else
+    {
       cout << "Something is very broken!" << endl;
-    list[i].setInclination(store);
+    }
+    
+    numCustomers++;
   }
-  while ((desperateCustomers != 19) && (cycles != 20))
+
+  while(numCustomers > 1 && numCycles < 20)
+  {
+    for(int i = 0; i < numCustomers; i++)
+    {
+      if(list[i].getInclination() == MOES_BAR)
+      {
+        MoesBar.addCustomer(list[i]);
+      }
+      else if(list[i].getInclination() == COMIC_BOOK_STORE)
+      {
+        CBGStore.addCustomer(list[i]);
+      }
+    }
+    
+    numCustomers = 0;
+
+    MoesBar.sell_stuff();
+    CBGStore.sell_stuff();
+
+    numCustomers += MoesBar.customers_leave(list, 0);
+    numCustomers += CBGStore.customers_leave(list, NUM_OF_CUSTOMERS - numCustomers);  
+ 
+    random_shuffle(&list[0], &list[NUM_OF_CUSTOMERS]);
+ 
+    Customer other;
+    for(int i = 0; i < numCustomers; i++)
+    {
+      other = list[rand() % numCustomers];
+
+      if(list[i].hasSameInclination(other))
+      {
+        list[i].rob(other);
+      }
+      else
+      {
+        list[i].throwSomething(other);
+      }
+    }
+    
+    for(int i = numCustomers - 1; i > 0; i--)
+    {
+      if(list[i].getHappiness() < 10 || list[i].getHappiness() > 90)
+      {
+        removeIndex(list, i, numCustomers);
+        numCustomers--;
+        cout << list[i].getName() << " is gone. There are now " << numCustomers << " people left." << endl;
+      }
+    }
+
+    numCycles++;
+  }
+  
+  /*while ((desperateCustomers != 19) && (cycles != 20))
   {
     MoeBar.sell_stuff();
     CBGStore.sell_stuff();
@@ -56,10 +106,11 @@ int main()
     MoeBar.customers_leave(list, MoeBar.getNumCustomers());
     CBGStore.customers_leave(list, CBGStore.getNumCustomers());
   //  random_shuffle(&list[0], &list[NUM_OF_CUSTOMERS]);
+
     for (int i = 0; i < NUM_OF_CUSTOMERS; i++)
     {
       here = true;
-      for(int j=0; j<desperateCustomers; j++)
+      for(int j = 0; j < desperateCustomers; j++)
       {
         if (list[i].getName()==listOfDesperation[j].getName())
           here = false;
@@ -122,7 +173,7 @@ int main()
       }
     }
     cycles++;
-  }
+  }*/
   //Sorting
   
 /*  for (int j=0; j < NUM_OF_CUSTOMERS; j++)
@@ -142,4 +193,12 @@ int main()
     cout << list[i] << endl;
   }*/
   return 0;
+}
+
+void removeIndex(Customer list[], const int index, const int size)
+{
+  for(int i = index; i < size - 1; i++)
+  {
+    list[i] = list[i + 1]; 
+  }
 }
