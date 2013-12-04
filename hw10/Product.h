1d64751ca2abbacc92d4306ef23bc3e09504bb45
Jordan Giacone <Jmgiacone@gmail.com>
2013-12-04 12:07:23 -0600

    Added documentation to the << operator

--- hw10/Product.h
+++ hw10/Product.h
@@ -16,6 +16,16 @@ struct Product
 {
   string m_name;
   float m_price;
+  
+  /*
+   * Description: An overloaded '<<' operator for the Product struct
+   * Pre: The output stream must be opened somewhere
+   * Params:
+      - out: The output stream to be used
+      - p: The product to be output
+   * Post: The output stream will be changed
+   * Return: The given output stream
+   */
   friend ostream& operator << (ostream& out, const Product p);
 };
 #endif
